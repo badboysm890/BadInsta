fcd4566019572d898b872bf9768800236ba49906
BadBoy17G <praveensm890@gmail.com>
2022-11-05 15:26:05 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
+$('.add-gif').on('click', function() {
+   
+     let gif = $('.gif-link').val();
+     $.ajax({
+      url: 'http://localhost:5000/add?gif=' + gif,
+      type: 'GET',
+      success: function(data) {
+        window.location.href = 'http://localhost:5500';
+      }
+  });
+});
+
+
+$(document).on('click', '.count', function() {
+  let count = $(this).text();
+    count++;
+    $(this).text(count);
+});
+
+// create an ajax request to localhost:5000
+$.ajax({
+    url: 'http://localhost:5000',
+    type: 'GET',
+    success: function(data) {
+      data.forEach(element => {
+        let str = `<div class="card1 border m-2">
+        <img class="card-img-top" src="${element.link}" alt="Card image" style="width:100%">
+        <div class="card-body">
+          <h4 class="card-title name">Gif By BB17G</h4>
+          <span><span class="count">0</span> Likes ❤️</span>
+        </div>
+      </div>`
+       $('.gifs').append(str);
+        });
+    }
+});
+
 
---
+++ main.py
@@ -0,0 +1,30 @@
+from flask import Flask, jsonify,request
+from flask_cors import CORS
+import json
+import pymongo
+import urllib
+
+app = Flask(__name__)
+CORS(app)
+client = pymongo.MongoClient("mongodb+srv://instacluster:insta123@instacluster.iapeyji.mongodb.net/?retryWrites=true&w=majority")
+db = client.insta
+collection = db.gif
+
+
+@app.route("/")
+def hello_world():
+    cursor = collection.find({}, {"_id": 0})
+    documents = []
+    for document in cursor:
+        documents.append(document["data"])
+    return jsonify(documents)
+
+@app.route("/add")
+def add():
+    gif = urllib.parse.unquote(request.args.get("gif"))
+    collection.insert_one({"data": {"link": gif}})
+    return "success"
+
+
+
+app.run(debug=True)   
